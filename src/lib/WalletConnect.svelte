e9ac2daf7fc35624de16c4acb63697b03a28fa28
Richard Gottleber <richard@gottleber.net>
2022-07-25 17:43:37 -0500

    remove step log

--- src/lib/WalletConnect.svelte
+++ src/lib/WalletConnect.svelte
@@ -16,17 +16,7 @@
 		const chainId = await signer.getChainId();
 		const contract = '';
 		web3Props = { signer, provider, chainId, account };
-		console.log(web3Props);
-	}
-	async function signAndVerify() {
-		let nonce = Math.floor(Math.random() * 1000000);
-		let signed = web3Props.signer.signMessage(`Signing one-time nonce: ${nonce}`);
-		console.log('MSG: ', await signed);
-		let valid =
-			ethers.utils.verifyMessage(`Signing one-time nonce: ${nonce}`, await signed) ===
-			web3Props.account;
-		console.log('VALID: ', valid);
-	}
+	}}
 </script>
 
 <div class="grid h-screen place-items-center">
